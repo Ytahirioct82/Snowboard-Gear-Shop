566e745734501ae06fe529208dbee297bce35556
Younes Tahiri <you@example.com>
2022-02-17 17:50:15 -0500

    heroku deployment

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,6 +1,13 @@
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS snowboard;
 
-CREATE TABLE test (
+CREATE TABLE snowboard (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    category TEXT NOT NULL,
+    name TEXT NOT NULL,
+    img TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price INT DEFAULT 0,
+    rating INT DEFAULT 0,
+    featured BOOLEAN DEFAULT false
+
 );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,6 @@
 \c cta_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO snowboard (category, name, img, description, price, rating, featured) VALUES
+('Snowboard', 'Salomon Bellevue', 'https://images.evo.com/imgp/250/207264/904374/clone.jpg', 'The Salomon Bellevue Splitboard is engineered for the adventurous rider seeking a light but capable setup for journeys into the wilderness white room.', 799, 3, false),
+('Snowboard', 'Jones Dream Catcher', 'https://images.evo.com/imgp/250/206659/818794/jones-dream-catcher-snowboard-women-s-2022-.jpg', 'In a world of increasing specialization, it is nice to get a simple one-step solution every now and then. If you are looking for a good time on the mountain.', 599, 5, true);
+
